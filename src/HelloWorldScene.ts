9047c5a626fdb055a3954d8620fd4dc69a493386
Joe Davey <crockettsoundlabs@gmail.com>
2025-04-24 11:47:27 -0700

    Simplify HelloWorldScene to fix TypeScript errors

--- src/HelloWorldScene.ts
+++ src/HelloWorldScene.ts
@@ -10,22 +10,10 @@ export default class HelloWorldScene extends Phaser.Scene {
 
     this.load.image('sky', 'assets/skies/space3.png')
     this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-    this.load.image('red', 'assets/particles/red.png')
   }
 
   create() {
     this.add.image(400, 300, 'sky')
-
-    // Create particles
-    const particles = this.add.particles(400, 300, 'red')
-
-    // Create emitter
-    const emitter = particles.createEmitter()
-
-    // Configure emitter
-    emitter.setSpeed(100)
-    emitter.setScale({ start: 1, end: 0 })
-    emitter.setBlendMode(Phaser.BlendModes.ADD)
 
     // Add logo
     const logo = this.physics.add.image(400, 100, 'logo')
@@ -34,8 +22,5 @@ export default class HelloWorldScene extends Phaser.Scene {
     logo.setVelocity(100, 200)
     logo.setBounce(1, 1)
     logo.setCollideWorldBounds(true)
-
-    // Make particles follow the logo
-    emitter.startFollow(logo)
   }
 }
